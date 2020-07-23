679962bb2fffaa1395defe20358fb370a7ebf251
David Lemayian <dlemayian@gmail.com>
2020-07-23 14:28:49 +0300

    Add title and favicons

---
+++ src/components/Head/Title.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+const Title = () => (
+  <div>
+    <Head>
+      <title>CWRA | Center for Women's Rights Advocacy</title>
+    </Head>
+  </div>
+)
+
+export default Title
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,14 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core'
+import { default as HeadTitle } from '../components/Head/Title'
+import { default as Favicons } from '../components/Head/Favicons'
 
 import theme from '../theme'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <HeadTitle/>
+      <Favicons/>
       <ColorModeProvider>
         <CSSReset />
         <Component {...pageProps} />
