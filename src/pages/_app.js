a4bbe9cb44aaed9c899bc914f942d9b7f8a91563
David Lemayian <dlemayian@gmail.com>
2020-07-23 23:14:53 +0300

    Adjust logo import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core'
 import { default as HeadTitle } from '../components/Head/Title'
-import { default as Favicons } from '../components/Head/Favicons'
+import { default as HeadFavicons } from '../components/Head/Favicons'
 
 import theme from '../theme'
 
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <HeadTitle/>
-      <Favicons/>
+      <HeadFavicons/>
       <ColorModeProvider>
         <CSSReset />
         <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
-import Logo from '../assets/svg/Logo.svg';
 import { Flex, Box } from "@chakra-ui/core";
 
 const Index = () => (
   <div>
-    <Flex flexDirection="column" alignItems="center" justifyContent="center" minHeight="100vh" bg="#0D78D3">
-      <Logo mx={2}/>
+    <Flex flexDirection="column" alignItems="center" justifyContent="center" minHeight="100vh" bg="#0D78D3" p={5}>
+      <img src="/images/logo.svg"/>
     </Flex>
   </div>
 )
