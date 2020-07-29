3ba3fc73d2043cad986c2cd7b726880667a4c625
David Lemayian <dlemayian@gmail.com>
2020-07-29 22:28:38 +0300

    Improve Footer.js

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,74 @@
-const {
+import { makeStyles } from '@material-ui/core/styles';
+import {
   Container, Box, Grid, Typography, Link,
-} = require('@material-ui/core');
+} from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
-const Footer = () => (
-  <footer>
-    <Container>
-      <Box borderTop={1} borderColor="grey.500" py={4}>
-        <Grid container>
-          <Grid item xs={6}>
-            <Typography>
-              © 2020 Center for Women&apos;s Rights Advocacy
-            </Typography>
+import { CwraTreeIcon } from './icons/CwraTreeIcon';
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    fontFamily: 'Playfair Display',
+    fontWeight: 'bold',
+  },
+}));
+
+const Footer = () => {
+  const classes = useStyles();
+  return (
+    <footer>
+      <Container>
+        <Box pt={4} pb={2}>
+          <Grid container>
+            <Grid item xs={12} sm="auto">
+              <Grid container spacing={1} justify="center">
+                <Grid item>
+                  <CwraTreeIcon style={{ color: 'inherit' }} fontSize="large" />
+                </Grid>
+                <Grid item>
+                  <Typography variant="h5" align="center" className={classes.title}>
+                    CWRA
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} sm>
+              <Typography align="center">
+                ...
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm="auto">
+              <Typography align="center">
+                <IconButton href="https://facebook.com/CWRAdvocacy" target="_blank" rel="noopener noreferrer">
+                  <FacebookIcon />
+                </IconButton>
+                {' '}
+                <IconButton href="https://twitter.com/CWRAdvocacy" target="_blank" rel="noopener noreferrer">
+                  <TwitterIcon />
+                </IconButton>
+                {' '}
+                <IconButton href="https://www.linkedin.com/company/cwradvocacy" target="_blank" rel="noopener noreferrer">
+                  <LinkedInIcon />
+                </IconButton>
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid item xs={6} align="right">
-            <Link>Facebook</Link>
-            {' . '}
-            <Link>Twitter</Link>
-            {' . '}
-            <Link>LinkedIn</Link>
-          </Grid>
-        </Grid>
-      </Box>
-    </Container>
-  </footer>
-);
+        </Box>
+        <Box borderTop={1} borderColor="grey.400" pt={2} pb={4} align="center">
+          <Typography variant="caption">
+            ©
+            {' '}
+            {new Date().getFullYear()}
+            {' '}
+            Center for Women&apos;s Rights Advocacy
+          </Typography>
+        </Box>
+      </Container>
+    </footer>
+  );
+};
 
 export default Footer;
