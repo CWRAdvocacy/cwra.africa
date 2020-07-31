96bb080ccdf82f6a2fe3206a1f718d83bebc0316
David Lemayian <dlemayian@gmail.com>
2020-07-31 11:47:13 +0300

    Add Twitter meta tags

--- src/components/Head/OpenGraph.jsx
+++ src/components/Head/OpenGraph.jsx
@@ -7,6 +7,10 @@ export const OpenGraph = () => (
     <meta property="og:url" content="https://cwra.africa/" key="og_url" />
     <meta property="og:image" content="https://cwra.africa/images/banners/home.jpg" key="og_image" />
     <meta property="og:description" content="CWRA is a non-profit organisation that seeks to promote gender equality by challenging harmful cultural practices and institutional norms at the root of all forms of violence against women and girls." key="og_description" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@CWRAdvocacy" />
+    <meta name="twitter:creator" content="@CWRAdvocacy" />
   </Head>
 );
 
